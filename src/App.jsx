820f13291ce27de34dcfcb1b49ddd6aba5a38a8b
Roman Zubin <utkabulka@gmail.com>
2023-12-19 22:46:17 +0400

    adding cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,56 @@ import DefaultPack from './DefaultPack'
 import Card from './components/Card'
 
 function App() {
-  const [pack, setPack] = useState(DefaultPack)
+  const [packInfo, setPackInfo] = useState({
+    pack_name: DefaultPack.pack_name,
+    language: DefaultPack.language,
+    author: DefaultPack.author,
+    card_color: DefaultPack.card_color,
+    text_color: DefaultPack.text_color,
+  })
+  const [cards, setCards] = useState(DefaultPack.cards)
+
+  function handleCardAdded() {
+    setCards([
+      ...cards,
+      {
+        id: crypto.randomUUID(),
+        text: 'Situation text',
+      },
+    ])
+  }
 
   function handleCardDeleted(cardId) {
-    let newPack = { ...pack }
-    newPack.cards = newPack.cards.filter((card) => {
-      if (card.id != cardId) return card
-    })
-    setPack(newPack)
+    setCards(
+      cards.filter((card) => {
+        if (card.id != cardId) return card
+      })
+    )
   }
 
   return (
     <>
-      <div className='header'></div>
+      <div className='header'>
+        <button>Import</button>
+        <button>Export</button>
+        <input placeholder='Pack name' />
+        <input placeholder='Author name' />
+      </div>
       <div className='cards'>
-        {pack.cards.map((card) => (
+        {cards.map((card) => (
           <Card
             key={card.id}
             id={card.id}
             text={card.text}
-            author={pack.author}
-            cardColor={pack.card_color}
-            textColor={pack.text_color}
+            author={packInfo.author}
+            cardColor={packInfo.card_color}
+            textColor={packInfo.text_color}
             onDelete={handleCardDeleted}
           />
         ))}
+        <div className='card add-card' onClick={handleCardAdded}>
+          +
+        </div>
       </div>
     </>
   )
